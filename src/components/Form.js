110b31b949a5da2a458f920f4ae80e46e405db4c
Sam Jespersen <heyheysamj@gmail.com>
2019-11-01 16:00:43 -0700

    api get and display functional

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,23 +5,21 @@ import PropTypes from 'prop-types';
 export default class Form extends Component {
 
     static propTypes = {
-      url: PropTypes.string.isRequired,
-      handleRouteChange: PropTypes.func.isRequired,
+      handleChange: PropTypes.func.isRequired,
       handleSubmit: PropTypes.func.isRequired,
-      handleUserInput: PropTypes.func.isRequired,
-      handleURLInput: PropTypes.func.isRequired
+
     }
 
     render() {
-      const { handleRouteChange, handleSubmit, handleUserInput, handleURLInput } = this.props;
+      const { handleSubmit, handleChange } = this.props;
       return (
         <form onSubmit={handleSubmit}>
-          <input type="string" placeholder="URL" onChange={handleURLInput} />
-          <input onChange={handleRouteChange} type="radio" name="method" value="get" /> <span>GET</span>
-          <input onChange={handleRouteChange} type="radio" name="method" value="post" /> <span>POST</span>
-          <input onChange={handleRouteChange} type="radio" name="method" value="put" /> <span>PUT</span>
-          <input onChange={handleRouteChange} type="radio" name="method" value="delete" /> <span>DELETE</span>
-          <textarea onChange={handleUserInput} placeholder="Raw JSON Body" rows={10} cols={70} />
+          <input type="string" name="url" placeholder="URL" onChange={handleChange} />
+          <input onChange={handleChange} type="radio" name="method" value="get" /> <span>GET</span>
+          <input onChange={handleChange} type="radio" name="method" value="post" /> <span>POST</span>
+          <input onChange={handleChange} type="radio" name="method" value="put" /> <span>PUT</span>
+          <input onChange={handleChange} type="radio" name="method" value="delete" /> <span>DELETE</span>
+          <textarea onChange={handleChange} name="userInput" placeholder="Raw JSON Body" rows={10} cols={70} />
           <button onClick={handleSubmit}>Go!</button>
         </form>
       );
