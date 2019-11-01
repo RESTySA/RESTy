702da306764a513e5f8d82be21b6a5dd20b6c61f
antonella <antonella.gr27@gmail.com>
2019-11-01 14:56:34 -0700

    worked on form chanhes & fetch

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './Header.js';
+import FormContainer from '../containers/FormContainer';
 
 const App = () => {
   return (
-        <>
-            <Header />
-        </>
+    <>
+      <Header />
+      <FormContainer />
+    </>
   );
 };
 
--- src/containers/FormContainer.js
+++ src/containers/FormContainer.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import Buttons from '../components/Buttons';
-import Results from '../components/Results';
-import UserInput from '../components/UserInput';
+// import Results from '../components/Results';
+import Form from '../components/Form';
+import { routeCall } from '../../services/APICalls';
 
-export default class Form extends Component {
+export default class FormContainer extends Component {
 
     state = {
       url: '',
@@ -15,13 +15,32 @@ export default class Form extends Component {
       event.preventDefault();
     }
 
+    handleRouteChange = ({ target }) => {
+      this.setState({ method: target.value });
+    }
+    handleUserInput = ({ target }) => {
+      this.setState({ userInput: target.value });
+    }
+    handleURLInput = ({ target }) => {
+      this.setState({ url: target.value });
+    }
+    handleSubmit= ({ target }) => {
+      event.preventDefault();
+      console.log(this.state);
+    }
     
 
 
     render() {
       return (
         <>
-  
+          <Form
+            url={this.state.url}
+            handleRouteChange={this.handleRouteChange}
+            handleSubmit={this.handleSubmit}
+            handleUserInput={this.handleUserInput}
+            handleURLInput={this.handleURLInput}
+          />
         </>
       );
     }
