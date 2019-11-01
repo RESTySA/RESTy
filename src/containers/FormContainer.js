5d83ad3fe3ccf20d1c591cb07f3dc5c6d9b28763
Sam Jespersen <heyheysamj@gmail.com>
2019-11-01 14:06:55 -0700

    some components!

---
+++ src/containers/FormContainer.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import Buttons from '../components/Buttons';
+import Results from '../components/Results';
+import UserInput from '../components/UserInput';
+
+export default class Form extends Component {
+
+    state = {
+      url: '',
+      method: '',
+      userInput: ''
+    }
+
+    componentDidUpdate() {
+      event.preventDefault();
+    }
+
+    
+
+
+    render() {
+      return (
+        <>
+  
+        </>
+      );
+    }
+}
