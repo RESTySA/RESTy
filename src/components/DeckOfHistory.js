4f55aac1f84c7b11cc14a66eb5153d4395cd90de
Sam Jespersen <heyheysamj@gmail.com>
2019-11-01 17:31:56 -0700

    history done!!

--- src/components/DeckOfHistory.js
+++ src/components/DeckOfHistory.js
@@ -2,25 +2,25 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import History from './History';
 
-const DeckOfHistory = (history) => {
-  // console.log(history.history);
+const DeckOfHistory = ({ history }) => {
   let historyCard;
   if(history.length > 0) {
-    console.log(history);
     historyCard = history.map(card => {
-      console.log(card);
       return (
         <li key={card.id}>
-          <History url={card.url}  method={card.method}/>
+          <History url={card.url} method={card.method} />
         </li>
       );
     });
   }
 
   return (
-    <ul>
-      {historyCard}
-    </ul>
+    <>
+      <h1>History</h1>
+      <ul>
+        {historyCard}
+      </ul>
+    </>
   );
 };
 
--- src/components/History.js
+++ src/components/History.js
@@ -3,10 +3,10 @@ import PropTypes from 'prop-types';
 
 const History = ({ method, url }) => {
   return (
-    <li>
+    <>
       <h2>{method.toUpperCase()}</h2>
       <span>{url}</span>
-    </li>
+    </>
   );
 };
 
