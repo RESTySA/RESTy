8fe91c02ca5e1813d51d9d9ff6abe3ac750a35bc
Sam Jespersen <heyheysamj@gmail.com>
2019-11-04 15:41:51 -0800

    just some css

--- src/components/History.js
+++ src/components/History.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 const History = ({ method, url }) => {
   return (
     <>
-      <h2>{method.toUpperCase()}</h2>
+      <h4>{method.toUpperCase()}</h4>
       <span>{url}</span>
     </>
   );
